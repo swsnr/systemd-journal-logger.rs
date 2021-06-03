30415e620a382fb48c94c1331e7f75c0bef44b73
Sebastian Wiesner <sebastian@swsnr.de>
2021-06-03 10:37:15 +0200

    Merge integration tests

--- tests/log_to_journal.rs
+++ tests/log_to_journal.rs
@@ -6,14 +6,19 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+//! Test that we actually log to the systemd journal,
+//! and that systemd can pick all those messages up.log_
+
 #![deny(warnings, clippy::all)]
 
+use std::collections::HashMap;
 use std::process::Command;
 
-use log::{warn, LevelFilter};
+use log::{Level, Log, Record};
 use rand::distributions::Alphanumeric;
 use rand::Rng;
-use std::collections::HashMap;
+
+use systemd_journal_logger::{JournalLog, LOG};
 
 fn random_target(prefix: &str) -> String {
     format!(
@@ -49,26 +54,32 @@ fn read_from_journal(target: &str) -> Vec<HashMap<String, String>> {
 
 #[test]
 fn simple_log_entry() {
-    systemd_journal_logger::init().ok();
-    log::set_max_level(LevelFilter::Info);
-
     let target = random_target("systemd_journal_logger/simple_log_entry");
 
-    warn!(target: &target, "systemd_journal_logger test: {}", 42);
+    LOG.log(
+        &Record::builder()
+            .level(Level::Warn)
+            .target(&target)
+            .module_path(Some(module_path!()))
+            .file(Some(file!()))
+            .line(Some(92749))
+            .args(format_args!("systemd_journal_logger test: {}", 42))
+            .build(),
+    );
 
     let entries = read_from_journal(&target);
     assert_eq!(entries.len(), 1);
     let entry = &entries[0];
 
+    assert_eq!(entry["TARGET"], target);
     assert_eq!(
         entry["PRIORITY"],
         u8::from(libsystemd::logging::Priority::Warning).to_string()
     );
     assert_eq!(entry["MESSAGE"], "systemd_journal_logger test: 42");
     assert_eq!(entry["CODE_FILE"], file!());
-    assert_eq!(entry["CODE_LINE"], "57");
+    assert_eq!(entry["CODE_LINE"], "92749");
     assert_eq!(entry["MODULE_PATH"], module_path!());
-    assert_eq!(entry["TARGET"], target);
 
     assert!(entry["SYSLOG_IDENTIFIER"].contains("log_to_journal"));
     assert_eq!(
@@ -88,29 +99,57 @@ fn simple_log_entry() {
 
 #[test]
 fn multiline_message() {
-    systemd_journal_logger::init().ok();
-    log::set_max_level(LevelFilter::Info);
-
     let target = random_target("systemd_journal_logger/multiline_message");
 
-    warn!(
-        target: &target,
-        "systemd_journal_logger test\nwith\nline {}", "breaks"
+    LOG.log(
+        &Record::builder()
+            .level(Level::Error)
+            .target(&target)
+            .module_path(Some(module_path!()))
+            .args(format_args!(
+                "systemd_journal_logger test\nwith\nline {}",
+                "breaks"
+            ))
+            .build(),
     );
 
     let entries = read_from_journal(&target);
     assert_eq!(entries.len(), 1);
     let entry = &entries[0];
 
+    assert_eq!(entry["TARGET"], target);
     assert_eq!(
         entry["PRIORITY"],
-        u8::from(libsystemd::logging::Priority::Warning).to_string()
+        u8::from(libsystemd::logging::Priority::Error).to_string()
     );
     assert_eq!(
         entry["MESSAGE"],
         "systemd_journal_logger test\nwith\nline breaks"
     );
-    assert_eq!(entry["CODE_FILE"], file!());
-    assert_eq!(entry["CODE_LINE"], "96");
+}
+
+#[test]
+fn extra_fields() {
+    let target = random_target("systemd_journal_logger/log_extra_field");
+
+    JournalLog::with_extra_fields(vec![("FOO", "BAR")]).log(
+        &Record::builder()
+            .level(Level::Debug)
+            .target(&target)
+            .module_path(Some(module_path!()))
+            .args(format_args!("with an extra field"))
+            .build(),
+    );
+
+    let entries = read_from_journal(&target);
+    assert_eq!(entries.len(), 1);
+    let entry = &entries[0];
+
     assert_eq!(entry["TARGET"], target);
+    assert_eq!(
+        entry["PRIORITY"],
+        u8::from(libsystemd::logging::Priority::Info).to_string()
+    );
+    assert_eq!(entry["MESSAGE"], "with an extra field");
+    assert_eq!(entry["FOO"], "BAR")
 }
